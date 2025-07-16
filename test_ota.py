6b23681950933396ae99987ddd08a6dc253ca1af
mmaz73 <mazzante@gmail.com>
2025-07-16 19:42:20 +0200

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -3,6 +3,6 @@ from ota import OTAUpdater
 
 firmware_url = "https://github.com/mmaz73/ota_upython_atag_q_display/main/"
 
-ota_updater = OTAUpdater(firmware_url, "test_ota.py")
+ota_updater = OTAUpdater(firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
