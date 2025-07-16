6ec994b294698ac93b590e31dd96104affbfb1d7
mmaz73 <mazzante@gmail.com>
2025-07-16 23:19:28 +0200

    Delete test_ota.py

--- test_ota.py
+++
@@ -1,8 +0,0 @@
-from ota import OTAUpdater
-# from WIFI_CONFIG import SSID, PASSWORD
-
-firmware_url = "https://github.com/mmaz73/ota_upython_atag_q_display/main/"
-
-ota_updater = OTAUpdater(firmware_url, "main.py")
-
-ota_updater.download_and_install_update_if_available()
